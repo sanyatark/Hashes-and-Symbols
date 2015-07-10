895515934a7cd2ab6640353467536ab89899c6ba
Tark <sanyatark@gmail.com>
2015-07-11 00:22:35 +0800

    Create script.rb

---
+++ script.rb
@@ -0,0 +1,13 @@
+movie_ratings = {
+  memento: 3,
+  primer: 3.5,
+  the_matrix: 3,
+  truman_show: 4,
+  red_dawn: 1.5,
+  skyfall: 4,
+  alex_cross: 2,
+  uhf: 1,
+  lion_king: 3.5
+}
+movie_ratings.each_key {|movie| puts movie, " " } # List output by movie title
+movie_ratings.each_value {|movie| puts movie, " " } # List output by rating value
